36cf16024141ba23ee873f6e9264d05033ab68d3
Wilkor <wilkorguitarrista@msn.com>
2021-07-16 21:00:09 -0300

    Initial commit

---
+++ precache-manifest.6a12237252e0085188dc812871da551b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a0e279a3d5080f7041bbad4e771de1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f33aa480ad68b2b0886",
+    "url": "./static/css/2.c0c2ff58.chunk.css"
+  },
+  {
+    "revision": "7d8f10521a663ee3c48b",
+    "url": "./static/css/main.e2ddf657.chunk.css"
+  },
+  {
+    "revision": "9f33aa480ad68b2b0886",
+    "url": "./static/js/2.0b6abbed.chunk.js"
+  },
+  {
+    "revision": "41368474946f96415743f45842c8bcbb",
+    "url": "./static/js/2.0b6abbed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8f10521a663ee3c48b",
+    "url": "./static/js/main.aae1d220.chunk.js"
+  },
+  {
+    "revision": "f36a3b9657e425b60302",
+    "url": "./static/js/runtime-main.637ebe85.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
