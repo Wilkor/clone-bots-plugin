1d48eefbcc5c0509afa1851da0fd8332b8eec213
Wilkor <wilkorguitarrista@msn.com>
2022-03-13 20:07:36 -0300

    melhorias
    melhorando o disparo do broadcast e  pegando informações dos subbots

---
+++ precache-manifest.59a319d9c42eef24c1abc44c45e1f45e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb496853e9c8290f730b9bf9df76755e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46a56e7558c8f2d25b00",
+    "url": "./static/css/2.c0c2ff58.chunk.css"
+  },
+  {
+    "revision": "9c1f23a9af5068d21469",
+    "url": "./static/css/main.ab44527d.chunk.css"
+  },
+  {
+    "revision": "46a56e7558c8f2d25b00",
+    "url": "./static/js/2.9692e98b.chunk.js"
+  },
+  {
+    "revision": "41368474946f96415743f45842c8bcbb",
+    "url": "./static/js/2.9692e98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1f23a9af5068d21469",
+    "url": "./static/js/main.2009e40b.chunk.js"
+  },
+  {
+    "revision": "f36a3b9657e425b60302",
+    "url": "./static/js/runtime-main.637ebe85.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.039fca1dfed34b0882d5e14622ec1be0.js"
+  "./precache-manifest.59a319d9c42eef24c1abc44c45e1f45e.js"
 );
 
 self.addEventListener('message', (event) => {
